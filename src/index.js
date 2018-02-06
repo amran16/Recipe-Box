0a6ff58ed58bc14c7288ec176f0afb5dd4e1ad68
amran16 <maryamg99@gmail.com>
2018-02-05 23:40:27 -0800

    added more detail

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style/index.css';
-import App from './components/App';
+import RecipeApp from './components/RecipeApp';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<RecipeApp />, document.getElementById('root'));
 registerServiceWorker();
